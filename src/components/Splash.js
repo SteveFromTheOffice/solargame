7468df7f91991acf81ce61d8f61e18beb23d843d
Michael Scott <mike.w.scott@mail.com>
2022-11-01 12:32:29 -0700

    Clean up

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -4,9 +4,7 @@ import { useSelector } from "react-redux";
 import "../styles/Splash.css";
 
 function Splash(props) {
-  const isVisible = useSelector(
-    (state) => state.sceneManager.isVisible["Splash"]
-  );
+  const isVisible = useSelector((state) => state.sceneManager.isVisible["Splash"]);
 
   return (
     <div className="Splash" style={{ display: isVisible ? "flex" : "none" }}>
